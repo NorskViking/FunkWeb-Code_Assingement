af2f126524431441fd212ad28f82aa82dc63d656
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-02-12 10:31:36 +0100

    First data request successfull
    Successfully made the first request for weather data for Oslo to the met.no api.

--- Pyhon Weather Forecast/Weather_Forecast.py
+++ Pyhon Weather Forecast/Weather_Forecast.py
@@ -3,20 +3,36 @@
 import datetime as dt
 import requests
 import json
+import simplejson
 
-BASE_URL = "https://api.met.no/weatherapi/locationforecast/2.0/compact?"
+# Open JSON data over Norwegian cities
+nor_cities = open('nor.json')
+# Convert JSON object to dict
+nor_data = json.load(nor_cities)
+# Close JSON file
+nor_cities.close()
+
+OSLO = ""
+# Get coordinates for Oslo
+for i in nor_data:
+    if i['city'] == 'Oslo':
+        OSLO = "lat="+str(i['lat'])+"&lon="+str(i['lon'])
+
+
+BASE_URL = f"https://api.met.no/weatherapi/locationforecast/2.0/compact?"
 USER_AGENT = {"User-Agent": "Weather_ForeCast jorgen@funkweb.org"}
-CITY = "Oslo"
-OSLO = "lat=59.91273&lon=10.74609"
 
-url = BASE_URL
+url = BASE_URL+OSLO
 
-response = requests.get(url).json()
-print(response.status_code)
+weather_data = requests.get(url, headers=USER_AGENT).json()
 
-data = response.text
+#print(first_req)
 
-x = '{"name":"Jørgen", "age":31, "city":"Oslo}'
-# parse_json = json.loads(x)
+# Store weather_data in local JSON file.
+with open('weather.json', 'w') as json_file:
+    json_file.write(simplejson.dumps(weather_data, indent=4))
 
-print(x)
+#response = requests.get(url).json()
+#print(response.status_code)
+
+#data = response.text
